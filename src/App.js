8942182f654674878b6710611eeb27d22b50169c
bunnysunny24 <bhavashesh@gmail.com>
2025-05-07 11:22:41 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Background from './components/Background';
+import About from './components/About';
+import Works from './components/Works';
+import Skills from './components/Skills';
+import Testimonials from './components/Testimonials';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -10,6 +16,12 @@ function App() {
       <div className="relative z-10 px-4 md:px-12 lg:px-20">
         <Navbar />
         <Hero />
+        <Works />z
+        <About />
+        <Skills />
+        <Testimonials />
+        <Contact />
+        <Footer />
       </div>
     </div>
   );
